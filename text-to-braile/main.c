60245e3a6f1a8dd0fab01020eb04cea783158a2d
nkdem <dev@nkdem.net>
2024-02-02 12:22:06 +0000

    Add grade selector

--- text-to-braile/main.c
+++ text-to-braile/main.c
@@ -16,15 +16,23 @@ void toWideChar(const char *str, widechar *wstr) {
 int main(int argc, char *argv[]) {
   lou_setLogLevel(LOU_LOG_OFF);
   setlocale(LC_ALL, "");
-  const char *tableList = "en-GB-g2.ctb";
-  const char *str = argv[1];
-  // set the length of inbuf to the length of the string
+  const int grade = argv[1][0] - '0';
+  const char *tableList;
+  if (grade == 1) {
+    tableList = "en-gb-g1.utb";
+  }
+  if (grade == 2) {
+    tableList = "en-GB-g2.ctb";
+  }
+  const char *str = argv[2];
   widechar inbuf[strlen(str)];
   toWideChar(str, inbuf);
+
   int inlen = strlen(str);
   widechar outbuf[strlen(str) * 2];
   int outlen = 100;
   int mode = 68;
+
   int err = lou_translateString(tableList, inbuf, &inlen, outbuf, &outlen, NULL,
                                 NULL, mode);
   if (err == 0) {
