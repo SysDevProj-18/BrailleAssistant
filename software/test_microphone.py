8844024503126a14e97bae29e286919aa74b5232
nkdem <dev@nkdem.net>
2024-02-08 13:50:47 +0000

    Remove temporary optimisation

--- software/test_microphone.py
+++ software/test_microphone.py
@@ -35,8 +35,6 @@ async def run_test():
                 text = json.loads(text)
                 if 'result' in text:
                     word = text['result'][0]['word']
-                    if word == "his":
-                        word = "is"
                     print(f'{word} -> {text_to_braille(word)}')
                     # clear audio queue (we only care about the first word)
                     while not audio_queue.empty():
