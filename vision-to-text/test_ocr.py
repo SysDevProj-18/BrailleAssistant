24788f91a44991dd8d741eaa08d806972634c6b6
Nikita Rameshkumar <nikitaramesh523@gmail.com>
2024-02-13 22:07:36 +0000

    Integrate webcam
    Integrate webcam with OCR. An image should be captured when the program is run and stored, and its text to be identified. Once the program starts, prompt the user to press the space bar once they are ready to scan, wait for 30 seconds and scan by default after timeout. Clean the code as well by separating into functions.

--- vision-to-text/test_ocr.py
+++ vision-to-text/test_ocr.py
@@ -1,13 +1,54 @@
 import easyocr
+import cv2
 import os
+import keyboard
 
-image_path = os.path.join(os.getcwd(),'images')
-os.chdir(image_path)
-reader = easyocr.Reader(['ch_sim','en'], gpu=False) # Run only once to load the model into memory.
+LANGUAGES = ['ch_sim', 'en']
+CAM_PORT = 0
+IMG_DIR = 'webcam_image'
+IMG_NAME = "webcam_image.png"
+TIMEOUT = 30
 
-fileName = str(input("Enter filename: "))
-txt_result = reader.readtext(fileName)
+# Loads OCR language models into memory.
+def loadOcrReader(languages):
+    return easyocr.Reader(languages, gpu=False)  # Run only once.
 
-txt_lines = [line[1] for line in txt_result]
-txt = ' '.join(txt_lines)
-print(txt)
+# Creates webcame image directory.
+def createImageDir(img_dir):
+    image_path = os.path.join(os.getcwd(), img_dir)
+    os.makedirs(image_path, exist_ok=True)
+    return image_path
+
+def waitForSpace(timeout):
+    print("Press the space bar to scan your text.")
+    keyboard.wait(" ", timeout) # captures anyways after 30 seconds.
+
+def captureImage():
+    cap = cv2.VideoCapture(CAM_PORT) # Access webcam.
+    _, frame = cap.read() # Capture frame.
+    cap.release()
+    return frame
+
+# Save captured frame.
+def saveImage(image_path, frame):
+    image_incl_path = os.path.join(image_path, IMG_NAME)
+    cv2.imwrite(image_incl_path, frame)
+    return image_incl_path
+
+# Read text using EasyOCR.
+def readTxt(reader, image_incl_path):
+    txt_result = reader.readtext(image_incl_path)
+    txt_lines = [line[1] for line in txt_result]
+    txt = ' '.join(txt_lines)
+    print(f"Text from {image_incl_path}: {txt}")
+
+def main():
+    reader = loadOcrReader(LANGUAGES)
+    image_path = createImageDir(IMG_DIR)
+    waitForSpace(TIMEOUT)
+    image = captureImage()
+    image_incl_path = saveImage(image_path, image)
+    readTxt(reader, image_incl_path)
+
+if __name__ == "__main__":
+    main()
