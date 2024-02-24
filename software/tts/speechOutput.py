27594d77d421cc1e75594fa6d880e11ee353b090
FL0W-1NG <floriansawicki@outlook.com>
2024-02-24 17:29:06 +0000

    split the keyboard and speech engines

---
+++ software/tts/speechOutput.py
@@ -0,0 +1,53 @@
+import pyttsx3
+import queue
+
+class SpeechOutput():
+
+    def __init__(self):
+
+        self.engine = pyttsx3.init()
+        self.queue = queue.Queue() 
+        self.volume = 1   # from 0.0 to 1.0
+        self.rate = 200   # default is 200 wpm
+
+    def setVolume(self, volume):
+
+        self.volume = volume
+
+    def setRate(self, rate):
+
+        self.rate = rate
+
+    def speechEngine(self, text):
+
+        self.engine = pyttsx3.init()
+
+        try:
+
+            self.engine.endLoop()
+            del self.engine
+            self.engine = pyttsx3.init()
+
+        except:
+
+            pass
+            self.engine.startLoop()
+            self.engine.say(text)
+
+    def loop(self):
+
+        while True:
+
+            if not self.queue.empty():
+
+                text = self.queue.get()
+
+                self.speechEngine(text)
+
+    def speak(self, text):
+
+        self.queue.put(text)
+
+tts = SpeechOutput()
+tts.speak("Hello")
+tts.loop()
