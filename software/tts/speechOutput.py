12350f163ee10e06fb0e0476dbe0ebcb55357800
oxrush <91465493+oxrush@users.noreply.github.com>
2024-02-25 16:24:07 +0000

    refactor tts

--- software/tts/speechOutput.py
+++ software/tts/speechOutput.py
@@ -1,5 +1,7 @@
 import pyttsx3
 import queue
+import threading
+import time
 
 class SpeechOutput():
 
@@ -9,46 +11,39 @@ class SpeechOutput():
         self.queue = queue.Queue() 
         self.volume = 1   # from 0.0 to 1.0
         self.rate = 200   # default is 200 wpm
+        self.__request_thread_stop = False
+        
+    def __open__(self):
+        self.__speech_thread = threading.Thread(target=self.__run)
+        self.__speech_thread.start()
+
+    
+    def __close__(self):
+        self.__request_thread_stop = True
+
 
     def setVolume(self, volume):
-
         self.volume = volume
 
     def setRate(self, rate):
-
         self.rate = rate
 
-    def speechEngine(self, text):
-
-        self.engine = pyttsx3.init()
-
-        try:
-
-            self.engine.endLoop()
-            del self.engine
-            self.engine = pyttsx3.init()
-
-        except:
-
-            pass
-            self.engine.startLoop()
-            self.engine.say(text)
-
-    def loop(self):
-
-        while True:
-
+    def __run(self, text):
+        while not self.__request_thread_stop:
             if not self.queue.empty():
-
                 text = self.queue.get()
+                self.engine.say(text)
+                self.engine.runAndWait()
+            time.sleep(0.01)
+        
 
-                self.speechEngine(text)
+
 
     def speak(self, text):
 
         self.queue.put(text)
 
 if __name__ == "__main__":
-    tts = SpeechOutput()
-    tts.speak("Hello")
-    tts.loop()
+    with SpeechOutput() as tts:
+        tts.speak("Hello")
+        tts.speak("world")
