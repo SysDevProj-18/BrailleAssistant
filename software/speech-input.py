3540fe8506a0d9bda3c4f59f4a55c82847a89f32
oxrush <91465493+oxrush@users.noreply.github.com>
2024-02-25 16:00:46 +0000

    refactor speech recognition

--- software/speech-input.py
+++ software/speech-input.py
@@ -3,65 +3,41 @@
 import json
 import asyncio
 import websockets
-import logging
 import sounddevice as sd
-import argparse
 
-from demo1 import text_to_braille
-def int_or_str(text):
-    """Helper function for argument parsing."""
-    try:
-        return int(text)
-    except ValueError:
-        return text
-
-def callback(indata, frames, time, status):
-    """This is called (from a separate thread) for each audio block."""
-    loop.call_soon_threadsafe(audio_queue.put_nowait, bytes(indata))
+class SpeechRecogniser():
 
 
-async def run_test():
-    with sd.RawInputStream(samplerate=args.samplerate, blocksize = 4000, device=args.device, dtype='int16',
-                           channels=1, callback=callback) as device:
+    def __callback(self, indata, frames, time, status):
+        """This is called (from a separate thread) for each audio block."""
+        self.__loop.call_soon_threadsafe(self.__audio_queue.put_nowait, bytes(indata))
 
-        async with websockets.connect(args.uri) as websocket:
-            await websocket.send('{ "config" : { "sample_rate" : %d } }' % (device.samplerate))
-            while True:
-                data = await audio_queue.get()
-                await websocket.send(data)
-                text = (await websocket.recv())
-                text = json.loads(text)
-                if 'result' in text:
-                    text_line = text['text']
-                    braille_output = text_to_braille(text_line)
-                    return braille_output # Braille string output until pause detected.
-async def main():
 
-    global args
-    global loop
-    global audio_queue
+    async def listen(self):
+        self.__loop = asyncio.get_running_loop()    
+        with sd.RawInputStream(samplerate=self.__sampling_rate, blocksize = 4000, device=self.__device_id, dtype='int16',
+                               channels=1, callback=self.__callback) as device:
 
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument('-l', '--list-devices', action='store_true',
-                        help='show list of audio devices and exit')
-    args, remaining = parser.parse_known_args()
-    if args.list_devices:
-        print(sd.query_devices())
-        parser.exit(0)
-    parser = argparse.ArgumentParser(description="ASR Server",
-                                     formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     parents=[parser])
-    parser.add_argument('-u', '--uri', type=str, metavar='URL',
-                        help='Server URL', default='ws://localhost:2700')
-    parser.add_argument('-d', '--device', type=int_or_str,
-                        help='input device (numeric ID or substring)')
-    parser.add_argument('-r', '--samplerate', type=int, help='sampling rate', default=16000)
-    args = parser.parse_args(remaining)
-    loop = asyncio.get_running_loop()
-    audio_queue = asyncio.Queue()
+            async with websockets.connect(self.__vosk_uri) as websocket:
+                await websocket.send('{ "config" : { "sample_rate" : %d } }' % (device.samplerate))
+                while True:
+                    data = await self.__audio_queue.get()
+                    await websocket.send(data)
+                    text = (await websocket.recv())
+                    text = json.loads(text)
+                    if 'result' in text:
+                        return text['text']
 
-    logging.basicConfig(level=logging.INFO)
-    await run_test()
+
+    def __init__(self, device_id=0, vosk_uri='ws://localhost:2700', sampling_rate=16000):
+        self.__vosk_uri = vosk_uri
+        self.__sampling_rate = sampling_rate
+        self.__device_id = device_id
+        self.__audio_queue = asyncio.Queue()
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    sr = SpeechRecogniser()
+    print(f'devices {sd.query_devices()}')
+    asyncio.run(sr.listen())
+
