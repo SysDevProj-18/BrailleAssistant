241ace5665fe0c1d52650ab49cde3ade39230d99
nkdem <dev@nkdem.net>
2024-02-28 17:55:36 +0000

    Programs up until demo 2

--- software/data_structures/Rail.py
+++ software/data_structures/Rail.py
@@ -6,11 +6,12 @@ from .Wheel import Wheel
 os.environ["GPIOZERO_PIN_FACTORY"] = os.environ.get("GPIOZERO_PIN_FACTORY", "lgpio")
 
 GEAR_RATIO = 1
-DELAY = 0.001  # in seconds, lower it to increase the speed of the motor
+DELAY = 0.005  # in seconds, lower it to increase the speed of the motor
 DEGREE_PER_ROTATION = 1.8
 GAP_LEFT_RIGHT = 2.0
-INTER_CELL_GAP = 1 # number of steps for the small gap
-MULTI_CELL_GAP = 0.5 # number of steps for a big gap
+INTER_CELL_GAP = 21  # number of steps for the small gap
+MULTI_CELL_GAP = 7  # number of steps for a big gap
+
 
 class Rail:
     def __init__(
@@ -18,53 +19,44 @@ class Rail:
         motor_pins,
     ):
         self.motor_pins = [OutputDevice(pin) for pin in motor_pins]  # Control pins
-        self.pos = [0,0] # 0 for left and 1 for right
-        self.DIR = OutputDevice(motor_pins[0]) # left motor pin is the direction
-        self.STEP = OutputDevice(motor_pins[1]) #right motor pin is the direction
+        self.pos = [0, 0]  # 0 for left and 1 for right
         self.steps = 0
 
-
-
-    def move_to(self, pos: int , wheel: Wheel):
+    def move_to(self, pos: int, wheel: Wheel):
         # move forward or backward
-        if self.pos != pos:
-            print("Moving rail from ", self.pos, " to ", pos)
-            if self.pos < pos[0]:
-                self.steps = self.step_calculator(pos, wheel, True)
-                self._forward(self.steps)
+        print("Moving rail from ", self.pos, " to ", pos)
+        print(f"self.pos = {self.pos}" + " pos = " + f"{pos}")
+        if self.pos[0] < pos or self.pos[1] < wheel:
+            self.steps = self.step_calculator(pos, wheel, True)
+            print("Steps: ", self.steps)
+            self._forward(self.steps)
 
-            elif self.pos > pos[0]:
-                self.steps = self.step_calculator(pos, wheel, False)
-                self._backward(self.steps)
+        elif self.pos[0] > pos or self.pos[1] > wheel:
+            self.steps = self.step_calculator(pos, wheel, False)
+            self._backward(self.steps)
 
-            self.pos[0] = pos
-            self.pos[1] = wheel
+        self.pos[0] = pos
+        self.pos[1] = wheel
 
-    def step_calculator(self, pos: int, wheel:Wheel , dir: bool):
-        steps = INTER_CELL_GAP
-        if dir:
-            steps = MULTI_CELL_GAP * (pos[0] - pos)
-            steps = steps + INTER_CELL_GAP * abs(wheel - pos[1])
+    def step_calculator(self, pos: int, wheel: Wheel, dir: bool):
+        inter_gap = (wheel + pos) - (self.pos[0] + self.pos[1])
+        multi_cell_gap = self.pos[0] - pos
+        return abs(multi_cell_gap) * MULTI_CELL_GAP + abs(inter_gap) * INTER_CELL_GAP
 
-        else :
-            steps = MULTI_CELL_GAP * (pos - pos[0])
-            steps = steps + INTER_CELL_GAP * abs(wheel - pos[1])
-        return steps
-
-    def _forward(self,steps: int):
-        self.DIR.on()
-        for i in range(int):
-            self.STEP.on()
+    def _forward(self, steps: int):
+        self.motor_pins[0].on()
+        for _ in range(steps):
+            self.motor_pins[1].on()
             sleep(DELAY)
-            self.STEP.off()
+            self.motor_pins[1].off()
             sleep(DELAY)
 
-    def _backward(self,steps: int):
-        self.DIR.off()
-        for i in range(int):
-            self.STEP.on()
+    def _backward(self, steps: int):
+        self.motor_pins[0].off()
+        for _ in range(steps):
+            self.motor_pins[1].on()
             sleep(DELAY)
-            self.STEP.off()
+            self.motor_pins[1].off()
             sleep(DELAY)
 
 
