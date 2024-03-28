a89652c19d3eba29f000ccb28025d92c6e7e18eb
Poppy - <dog@poppyio.com>
2024-03-28 18:30:57 +0000

    Switched to more sophisticated GitHub actions nox handling. Added missing pytest install in noxfile.

--- software/noxfile.py
+++ software/noxfile.py
@@ -4,10 +4,11 @@ import nox
 @nox.session
 def test(session):
     session.install(".")
+    session.install("pytest")
     session.run("pytest")
 
 
 @nox.session
 def lint(session):
     session.install("ruff")
-    session.run("ruff", ".")
+    session.run("ruff", "check", ".")
